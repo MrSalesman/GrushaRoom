5e22c1f8b7bf330fcefac9d527ae10dbb0b45e44
student <student@1-DELL-AUD501>
2021-10-22 14:12:29 +0300

    Добавьте файлы проекта.

---
+++ GrushaRoom/Admin/AdminProduct.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GrushaRoom.Admin
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminProduct.xaml
+    /// </summary>
+    public partial class AdminProduct : Window
+    {
+        public AdminProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbFiltrationCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void tbPrice_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void tbDescription_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void tbMaterial_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnProductClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnProductSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
